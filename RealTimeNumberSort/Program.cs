37c92272b337f0ea92eb40d972d95842a5ee4f42
Debashis <jupiter_104@ymail.com>
2019-03-30 19:07:24 +0530

    Add files via upload

---
+++ RealTimeNumberSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RealTimeNumberSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            var unique = new List<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number or type 'quit' to exit");
+                string input = Console.ReadLine();
+
+                if (input == "quit")
+                {
+                    break;
+                }
+
+                int num = int.Parse(input);
+
+                numbers.Add(num);
+                numbers.Sort();
+                unique = numbers.Distinct().ToList();
+
+                Console.Write("Sorted and unique numbers: ");
+                foreach (var n in unique)
+                {
+                    Console.Write(n + ", ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
